1c8e956b4f86d551274db86106f0db60c59c050e
yangmingzi <120869049@qq.com>
2015-06-22 10:25:50 +0800

    commit

--- 214-ShortestPalindrome.cpp
+++ 214-ShortestPalindrome.cpp
@@ -16,18 +16,20 @@ public:
         }
         //p[i]-1恰好就是原字符串中的回文串长度
         vector<int> p(s1.length(), 0);
-        int res = 0;
-        for (int id = 0, i = 1; i < s1.length(); ++i) {
-            int mx = id+p[id];//最大回文子串的边界
+        int res = 0,mx=0,id=0;
+        for (int id = 0, i = 1; i < s1.length(); ++i) {           
             //选取i，如果不在当前最大回文内，则p[i]=1.若在，利用对称性获得临时的p[i]
             if (i < mx){ 
                 p[i] = min(p[2 * id - i], mx - i);//id表示最大回文子串的中心位置
             }
             else p[i] = 1;
             //扩展以p[i]为中心的回文数组
-            while (s1[i + p[i]] == s1[i - p[i]]) ++p[i];
+            while (s1[i + p[i]] == s1[i - p[i]]) ++p[i];         
+            if (mx < i + p[i]){
+                id = i;
+                mx = id+p[id];//最大回文子串的边界
+            }
             //所有回文串都以以第一个字符为起点，所以下面可以这样替换
-            if (mx < i + p[i]) id = i;
             if (p[i] == i) res = max(res, i);
         }
         //最终返回的是回文串终点在子串的位置
