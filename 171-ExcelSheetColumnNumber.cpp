e05a023d76ab2258be4dfc46823fb60f0d54e578
yangmingzi <mystudyingspace@163.com>
2015-06-24 00:12:42 +0800

    commit

---
+++ 171-ExcelSheetColumnNumber.cpp
@@ -0,0 +1,12 @@
+//26进制转化成十进制
+class Solution {
+public:
+    int titleToNumber(string s) {
+       int res=0;
+       for(int i=0;i<s.length();i++){
+           res*=26;
+           res+=(s[i]-'A'+1);
+       }
+       return res;
+    }
+};
