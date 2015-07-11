76008bf0555ee0bc5f65a258e43b2c7000c19b36
yangmingzi <mystudyingspace@163.com>
2015-07-11 16:16:06 +0800

    commit

---
+++ 73-Set Matrix Zeroes.cpp
@@ -0,0 +1,44 @@
+/*
+分析：
+O(mn)解法：用m*n的空间记录每个某个位置是否要设置成0。
+O(m+n)解法：用一个数组记录某一列是否需要设置成0，用一个数组记录某一行是否需要设置成0。
+常数空间呢？如果我们可以用matrix本身的空间来记录，就不需要额外的空间了。解法如下：
+
+用matrix第一行记录相应的列是都需要置0，第一列来记录相应的行是否需要置0。
+用来记录之前需要提前判断第一行和第一列是否需要置0。然后遍历matrix（出了第一行和第一列）
+如果matrix[i][j]等于0，那么matrixp[i][0] = 0 (行标志)，matrix[0][j] = 0(列标志)。
+再次遍历matrix只要对应位置的行标志或者列标志等于0，相应的位置就置0,。最后还要看第一行和第一列是否需要置0.
+*/
+class Solution {
+public:
+    void setZeroes(vector<vector<int> > &matrix) {
+        //使用第一行和第一列作为记录空间
+        int row = matrix.size();
+        if(row == 0)return;
+        int col = matrix[0].size();
+        bool firstRow = false, firstCol = false;//第一行和第一列是否要清0的标志
+        for(int i = 0; i < col; i++)
+            if(matrix[0][i] == 0){firstRow = true; break;}
+        for(int i = 0; i < row; i++)
+            if(matrix[i][0] == 0){firstCol = true; break;}
+             
+        for(int i = 1; i < row; i++)
+            for(int j = 1; j < col; j++)
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+        for(int i = 1; i < row; i++)
+            for(int j = 1; j < col; j++)
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+                     
+        if(firstRow)
+            for(int i = 0; i < col; i++)
+                matrix[0][i] = 0;
+        if(firstCol)
+            for(int i = 0; i < row; i++)
+                matrix[i][0] = 0;
+    }
+};
---
+++ 74-Search a 2D Matrix.cpp
@@ -0,0 +1,42 @@
+/*
+分析：
+分两步，
+（1）先二分搜索的元素定位到行：当目标小于第一列某个元素时，向前面的行中去搜索；
+当目标大于第一列某个元素分两种情况 a、大于该元素所在行的最后一个元素时，往后面的行中去搜索，
+b、小于等于该元素所在行的最后一个元素，则可以定位到该元素所在的行。
+（2）在定位好的行中二分搜索
+*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        int row = matrix.size();
+        if(row == 0)return false;
+        int col = matrix[0].size();
+        int low = 0, high = row - 1;
+        while(low < high)//注意这里没有=
+        {//二分查找定位行
+            int mid = (low + high) / 2;
+            if(target < matrix[mid][0])
+                high = mid - 1;
+            else if(target > matrix[mid][0])
+            {
+                if(target > matrix[mid][col - 1])
+                    low = mid + 1;
+                else {low = mid; break;}
+            }
+            else return true;
+        }
+        int k = low; //已经把数据定位在了第row行
+        low = 0;  high = col - 1;
+        while(low <= high)
+        {//行内二分查找
+            int mid = (low + high) / 2;
+            if(target < matrix[k][mid])
+                high = mid - 1;
+            else if(target > matrix[k][mid])
+                low = mid + 1;
+            else return true;
+        }
+        return false;
+    }
+};
