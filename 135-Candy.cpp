6f8fe7836caaf7d0256be1fa4c268fe111e7475d
yangmingzi <120869049@qq.com>
2015-08-05 21:22:07 +0800

    commit

--- 135-Candy.cpp
+++ 135-Candy.cpp
@@ -1,3 +1,12 @@
+/*
+There are N children standing in a line. Each child is assigned a rating value.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+*/
 //贪心算法
 //先让从左到右满足条件
 //再不改变从左到右的成果的前提下，是从右到左满足条件
