46d16420b850e5667877e45ac64eed2067ff284b
yangmingzi <120869049@qq.com>
2015-08-08 22:28:51 +0800

    commit

--- 82-Remove Duplicates from Sorted List II.cpp
+++ 82-Remove Duplicates from Sorted List II.cpp
@@ -6,6 +6,8 @@ For example,
 Given 1->2->3->3->4->4->5, return 1->2->5.
 Given 1->1->1->2->3, return 2->3.
 */
+//考察双指针
+//8ms
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -51,5 +53,39 @@ public:
         head = addHead->next;
         delete addHead;
         return head;
+    }
+};
+//8ms
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if ((head==NULL) || (head->next == NULL) ){return head;}
+        ListNode* pre = new ListNode(0);
+        pre->next = head;
+        head =pre;
+        ListNode* p = head;
+                    
+        while(p->next!=NULL){
+            ListNode *p2 = p->next;
+            while ((p2->next!=NULL)&&(p2->val==p2->next->val)){
+                p2=p2->next;
+            }
+            if (p2!=p->next){
+                p->next=p2->next;
+            }else{
+                p=p->next;
+            }
+        }  
+        return head->next;
     }
 };
