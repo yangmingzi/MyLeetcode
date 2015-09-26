1723a35079f082e51a43d820cd9f41d5fc424d02
yangmingzi <mystudyingspace@163.com>
2015-09-27 00:43:51 +0800

    commit

--- 215-Kth Largest Element in an Array.cpp
+++ 215-Kth Largest Element in an Array.cpp
@@ -25,3 +25,43 @@ int findKthLargest(vector<int>& nums, int k) {
 }
 };
 
+/**/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        vector<int> less;
+        vector<int> large;
+        int n = nums.size();
+        int ta = nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]>ta) large.push_back(nums[i]);
+            else if (nums[i]<ta) less.push_back(nums[i]);
+        }
+        if(large.size()>=k) return findKthLargest(large,k);
+        if(n-less.size()<k) return findKthLargest(less,k-(n-less.size()));
+        return ta;
+     }
+};
+/*O(1)空间复杂度*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int L=0,R=nums.size()-1;
+        while(L<R){
+            int left = L ,right = R;
+            int key  = nums[left];
+            while(left<right){
+                while(left<right && nums[right]<key) --right;
+                nums[left] = nums[right];
+                while(left<right && nums[left]>=key) ++left;
+                nums[right] = nums[left];
+            }
+            nums[left] = key;
+            if(left == k-1) return nums[k-1];
+            else if(left > k-1) R= left - 1;
+            else L = left + 1;
+        }
+        return nums[k-1];
+    }
+};
+
