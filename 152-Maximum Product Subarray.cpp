626e5f332a2c972158e2bfcf0cc55707834e0478
yangmingzi <mystudyingspace@163.com>
2015-09-27 00:45:54 +0800

    commit

--- 152-Maximum Product Subarray.cpp
+++ 152-Maximum Product Subarray.cpp
@@ -1,4 +1,10 @@
 /*
+Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+For example, given the array [2,3,-2,4],
+the contiguous subarray [2,3] has the largest product = 6.
+*/
+/*
 这道题跟Maximum Subarray模型上和思路上都比较类似，还是用一维动态规划中的“局部最优和全局最优法”。
 这里的区别是维护一个局部最优不足以求得后面的全局最优，这是由于乘法的性质不像加法那样，累加结果只要是正的一定是递增，
 乘法中有可能现在看起来小的一个负数，后面跟另一个负数相乘就会得到最大的乘积。不过事实上也没有麻烦很多，
@@ -18,5 +24,28 @@ public:
             res=max(res,tmp_max);
         }
         return res;
+    }
+};
+
+/**/
+
+#define MAX(x,y) ((x)>(y)?(x):(y))
+#define MIN(x,y) ((x)<(y)?(x):(y))
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=0) return 0;
+        if(n==1) return nums[0];
+        int max_local = nums[0];
+        int min_local = nums[0];
+        int global = nums[0];
+        for(int i=1;i!=n;++i){
+            int min_copy = min_local;
+            min_local = MIN(MIN(nums[i]*max_local ,nums[i]),nums[i]*min_local);
+            max_local = MAX(MAX(nums[i]*max_local,nums[i]),nums[i]*min_copy);
+            global = MAX(global,max_local);
+        }
+        return global;
     }
 };
