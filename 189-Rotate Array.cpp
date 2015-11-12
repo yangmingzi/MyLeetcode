6b68ab52f61f25cb6cf3992f94ae88fe903193d3
yangmingzi <mystudyingspace@163.com>
2015-11-12 20:34:58 +0800

    commit

--- 189-Rotate Array.cpp
+++ 189-Rotate Array.cpp
@@ -1,5 +1,13 @@
-//三种解法，第三种解法太牛逼了
-//参考博客：http://blog.csdn.net/foreverling/article/details/43925891
+/*
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+*/
+//三种解法,参考博客：http://blog.csdn.net/foreverling/article/details/43925891
+//24ms
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -14,5 +22,33 @@ public:
             nums[i] = temp[(i - k + n) % n];
         }
       
+    }
+};
+//24ms
+/*
+①将整个数组反转 
+②将由分割点分割的两个数组分别反转 
+即：1 2 3 4 5 6 7 -> 7 6 5 | 4 3 2 1 -> 5 6 7 | 1 2 3 4 
+时间复杂度为O(n)，空间复杂度为O(1)。
+*/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        rev(nums, 0, n - 1);
+        rev(nums, 0, k - 1);
+        rev(nums, k, n - 1);
+    }
+
+    void rev(vector<int>& nums, int left, int right)
+    {
+        int temp;
+        while (left < right)
+        {
+            temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
+        }
     }
 };
--- 201-Bitwise AND Of Numbers Range.cpp
+++ 201-Bitwise AND Of Numbers Range.cpp
@@ -1,4 +1,9 @@
 /*
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+For example, given the range [5, 7], you should return 4.
+*/
+/*
 我们再来看一个范围[26, 30]，它们的二进制如下
 11010　　11011　　11100　　11101　　11110
 发现了规律后，我们只要写代码找到左边公共的部分即可
