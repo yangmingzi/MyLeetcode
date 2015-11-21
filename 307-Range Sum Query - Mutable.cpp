6e78bbbf9a7ba60ace449ba3bf2e8d6e4f8c1dd8
yangmingzi <120869049@qq.com>
2015-11-21 11:55:14 +0800

    commit

--- 307-Range Sum Query - Mutable.cpp
+++ 307-Range Sum Query - Mutable.cpp
@@ -12,15 +12,80 @@ Note:
 The array is only modifiable by the update function.
 You may assume the number of calls to update and sumRange function is distributed evenly.
 */
-public class NumArray {
+//线段树
+struct SegmentTreeNode {
+    int start, end, sum;
+    SegmentTreeNode* left;
+    SegmentTreeNode* right;
+    SegmentTreeNode(int a, int b):start(a),end(b),sum(0),left(nullptr),right(nullptr){}
+};
+class NumArray {
+    SegmentTreeNode* root;
+public:
+    NumArray(vector<int> &nums) {
+        int n = nums.size();
+        root = buildTree(nums,0,n-1);
+    }
+
+    void update(int i, int val) {
+        modifyTree(i,val,root);
+    }
+
+    int sumRange(int i, int j) {
+        return queryTree(i, j, root);
+    }
+    SegmentTreeNode* buildTree(vector<int> &nums, int start, int end) {
+        if(start > end) return nullptr;
+        SegmentTreeNode* root = new SegmentTreeNode(start,end);
+        if(start == end) {
+            root->sum = nums[start];
+            return root;
+        }
+        int mid = start + (end - start) / 2;
+        root->left = buildTree(nums,start,mid);
+        root->right = buildTree(nums,mid+1,end);
+        root->sum = root->left->sum + root->right->sum;
+        return root;
+    }
+    int modifyTree(int i, int val, SegmentTreeNode* root) {
+        if(root == nullptr) return 0;
+        int diff;
+        if(root->start == i && root->end == i) {
+            diff = val - root->sum;
+            root->sum = val;
+            return diff;
+        }
+        int mid = (root->start + root->end) / 2;
+        if(i > mid) {
+            diff = modifyTree(i,val,root->right);
+        } else {
+            diff = modifyTree(i,val,root->left);
+        }
+        root->sum = root->sum + diff;
+        return diff;
+    }
+    int queryTree(int i, int j, SegmentTreeNode* root) {
+        if(root == nullptr) return 0;
+        if(root->start == i && root->end == j) return root->sum;
+        int mid = (root->start + root->end) / 2;
+        if(i > mid) return queryTree(i,j,root->right);
+        if(j <= mid) return queryTree(i,j,root->left);
+        return queryTree(i,mid,root->left) + queryTree(mid+1,j,root->right);
+    }
+};
+
+//
+class NumArray {
+public:
     vector<int> arr;
     vector<int> tree;
     int length;
-    public NumArray(vector<int>& nums) {
-        length = nums.length;
-        arr = nums;
-        tree = nums;
-        nums.push_back(0);
+    NumArray(vector<int> &nums) {
+        length = nums.size();
+        vector<int> tmp(length,0);
+        arr = tmp;
+        tree = tmp;
+        tree.push_back(0);
         for (int i = 0; i < length; i++) {
             update(i, nums[i]);
         }
@@ -36,7 +101,7 @@ public class NumArray {
         }
     }
 
-    public int sumRange(int i, int j) {
+    int sumRange(int i, int j) {
         int sumi = 0;
         int sumj = 0;
         j++;
@@ -52,4 +117,4 @@ public class NumArray {
         }
         return sumj - sumi;
     }
-}
+};
